8b544103218a0d40dedd13356a1bbd88c1ec1a13
pli233 <peiyuanli3627@gmail.com>
2025-12-05 04:32:49 -0500

    update url

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <HashRouter>
+        <BrowserRouter>
             <App />
-        </HashRouter>
+        </BrowserRouter>
     </StrictMode>
 );
